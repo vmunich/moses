839835b440d1b85807e31067d0bb4e7936d32f87
Münich <vmunich@users.noreply.github.com>
2018-06-24 21:32:05 -0400

    Update config.sh

--- modules/config.sh
+++ modules/config.sh
@@ -24,7 +24,7 @@ setup_environment()
 
 check_configuration()
 {
-    if [[ $network != 'mainnet' && $network != 'devnet' ]]; then
-        abort 1 "network [$network] is invalid."
+    if [[ $database_name == '' && $node_dir == '' ]]; then
+        abort 1 "Configuration is invalid."
     fi
 }
