a30ce0ba8083e3018d93ff0c9c77ad888bce282a
Münich <vmunich@users.noreply.github.com>
2018-06-24 23:03:06 -0400

    Update dump.sh

--- modules/dump.sh
+++ modules/dump.sh
@@ -12,6 +12,7 @@
 moses_dump()
 {
     # make sure postgres is running
+    info "Stopping PostgreSQL"
     process_postgres=$(pgrep -a postgres | awk '{print $1}')
 
     if [ -z "$process_postgres" ]; then
@@ -22,24 +23,30 @@ moses_dump()
     cd $snapshot_dir
 
     # create a new snapshot
+    info "Taking snapshot"
     pg_dump -Fc $database_name > latest
 
     # move old snapshot
+    info "Archiving older current snapshot"
     current_file=current # $(ls -t | head -n1)
     current_date=$(stat -c %Y $current_file)
     mv $current_file $current_date
 
     # move new snapshot
+    info "Updating current snapshot"
     mv latest $current_file
     
     # write the height of the last snapshot on index.html
     curl -s http://127.0.0.1:+$node_port/api/blocks/getHeight > index.html
 
     # delete all but the 10 recent snapshots
+    info "Deleting $snapshot_amount oldest snapshots"
     ls -t -I "*.html"| tail -n +$snapshot_amount | xargs rm --
 
     # check if we send the snapshot to a remote location
     if [[ $snapshot_remote = true ]]; then
         rsync --checksum --no-whole-file -v -e "ssh -p ${snapshot_remote_port} -i ${snapshot_remote_identity_file}" current "${snapshot_remote_user}@${snapshot_remote_host}:${snapshot_remote_directory}"
     fi
+    
+    success "Finished"
 }
